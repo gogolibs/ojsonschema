d3a95658f61ac39c7bf69555349509d59443a2fd
Kurkin <deemson.k@gmail.com>
2021-04-23 12:17:41 +0300

    beautified tests

--- ojsonschema_test.go
+++ ojsonschema_test.go
@@ -7,37 +7,42 @@ import (
 	"testing"
 )
 
-type testCase struct {
+var testCases = []struct {
+	name     string
 	expected string
 	actual   interface{}
-}
-
-var testCases = []testCase{
+}{
 	{
+		name: "string: simple",
 		expected: `{ "type": "string" }`,
 		actual:   ojsonschema.String{},
 	},
 	{
+		name: "string: format regex",
 		expected: `{ "type": "string", "format": "regex" }`,
 		actual:   ojsonschema.String{Format: "regex"},
 	},
 	{
+		name: "object: simple",
 		expected: `{ "type": "object" }`,
 		actual:   ojsonschema.Object{},
 	},
 	{
+		name: "object: additional properties flag",
 		expected: `{ "type": "object", "additionalProperties": false }`,
 		actual: ojsonschema.Object{
 			AdditionalProperties: false,
 		},
 	},
 	{
+		name: "array: strings",
 		expected: `{ "type": "array", "items": { "type": "string" } }`,
 		actual: ojsonschema.Array{
 			Items: ojsonschema.String{},
 		},
 	},
 	{
+		name: "oneOf: string, object",
 		expected: `{ "oneOf": [ { "type": "string" }, { "type": "object" }] }`,
 		actual: ojsonschema.OneOf(
 			ojsonschema.String{},
@@ -48,13 +53,15 @@ var testCases = []testCase{
 
 func TestCases(t *testing.T) {
 	for _, testCase := range testCases {
-		expectedI := new(interface{})
-		err := json.Unmarshal([]byte(testCase.expected), expectedI)
-		require.NoError(t, err)
-		expectedNormalizedData, err := json.Marshal(expectedI)
-		require.NoError(t, err)
-		actualData, err := json.Marshal(testCase.actual)
-		require.NoError(t, err)
-		require.Equal(t, string(expectedNormalizedData), string(actualData))
+		t.Run(testCase.name, func(t *testing.T) {
+			expectedI := new(interface{})
+			err := json.Unmarshal([]byte(testCase.expected), expectedI)
+			require.NoError(t, err)
+			expectedNormalizedData, err := json.Marshal(expectedI)
+			require.NoError(t, err)
+			actualData, err := json.Marshal(testCase.actual)
+			require.NoError(t, err)
+			require.Equal(t, string(expectedNormalizedData), string(actualData))
+		})
 	}
 }
